d1e7748d50cd7385abc576a914a73847f1894ee8
Yann Gauteron <yann@amigne.ovh>
2022-06-25 21:42:31 +0200

    Backend: Add authentication API

---
+++ webapp/src/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenRefreshView
+from . import views
+
+
+urlpatterns = [
+    path('token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes)
+]
